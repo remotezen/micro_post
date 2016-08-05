e9afc741d6752ad542c73f925ed3e06e2a79bcbb
Remotezen <remotezen@bell.net>
2016-08-05 15:56:07 -0400

    added contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/help', to: 'static_pages#home'
+  get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/help', to: 'static_pages#help'
+
   root 'static_pages#home'
 end
 
