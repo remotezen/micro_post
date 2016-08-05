f7ff937bc73b563408d2195cab05c6160dc07f5d
Remotezen <remotezen@bell.net>
2016-08-05 15:51:51 -0400

    sanity check

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def full_title(title)
+    unless title.blank?
+      title += " | Micropost Application"
+    else
+      title = "Micropost Application"
+    end
+  end
 end
