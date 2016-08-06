56ed5204acf2a1d4bf3ef165d5def8f6a1d00fe4
Remotezen <remotezen@bell.net>
2016-08-06 15:17:45 -0400

    finished with modeling_user

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails',        '5.0.0'
+gem 'bcrypt'
 gem 'pry'
 gem 'rails-pry'
 gem 'bootstrap-sass', '3.3.6'
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = users(:one)
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar",
+                    username: "example")
   end
+
   test "user should be valid" do
     assert @user.valid?
   end
@@ -62,6 +65,7 @@ class UserTest < ActiveSupport::TestCase
       @user.save
       assert_not duplicate_user.valid?
     end
+
     test "assert username is not case sensitive" do
       duplicate_user = @user.dup
       duplicate_user.email = "baz@bar.com"
@@ -76,4 +80,15 @@ class UserTest < ActiveSupport::TestCase
       @user.save
       assert_not duplicate_user.valid?
     end
+
+    test "password should be present (nonblank)" do
+      @user.password = @user.password_confirmation = " " * 6
+      assert_not @user.valid?
+    end
+
+    test "password should have a minimum length" do
+      @user.password = @user.password_confirmation = "a" * 5
+      assert_not @user.valid?
+    end
+
 end
