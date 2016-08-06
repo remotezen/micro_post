69356cb0b45b6751307c8077144d899bec343280
Remotezen <remotezen@bell.net>
2016-08-06 15:56:55 -0400

    added gravatar helper

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:one)
+  end
+
   test "should get new" do
     get signup_url
+    assert_response :success
+  end
+  test "should get show" do
+    get user_path(@user)
     assert_response :success
   end
 
