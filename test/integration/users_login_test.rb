a269306935229ee63f07720af523265eb627358a
Remotezen <remotezen@bell.net>
2016-08-18 08:08:10 -0400

    added some user login tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -19,6 +19,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
                                       password: 'password'  }}
 
     assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
 
   end
 end
