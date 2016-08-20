8feb8d2cbe9b16fee05fdce1a8550d44181c62cd
Remotezen <remotezen@bell.net>
2016-08-20 13:22:52 -0400

    finished with user delete links tests and user-update

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:one)
+    @other = users(:one)
+    @user = users(:fred)
   end
 
   test "should get new" do
@@ -17,6 +18,27 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should not redirect to users/index when not logged in" do
     get users_path
     assert_redirected_to login_url
+  end
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+
+  end
+  test "should should redirect destroy for non admin user" do
+    log_in_as(@other)
+    assert_no_difference 'User.count' do
+      delete user_path(@other)
+    end
+    assert_redirected_to root_url
+  end
+  test "admin should be able to delete user" do
+    log_in_as(@user)
+    assert_difference 'User.count', -1  do
+      delete user_path(@other)
+    end
+    assert_redirected_to users_path
   end
 
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,7 +2,10 @@ require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
+    #this is an admin user
     @user = users(:fred)
+    #non-admin user
+    @other = users(:one)
   end
   test "index including pagination" do
     log_in_as(@user)
@@ -14,6 +17,27 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     User.paginate(page: 1).each do |u|
       assert_select 'a[href=?]', user_path(u), text: u.username
     end
+  end
+  test "index as admin including pagination and delete links" do
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |u|
+      assert_select 'a[href=?]', user_path(u), text:u.username
+      unless u == @user
+        assert_select 'a[href=?]', user_path(u), text: 'delete'
+      end
+    end
+    assert_difference 'User.count', -1 do
+      delete user_path(@other)
+    end
+  end
+  test "index as non-admin" do
+    log_in_as(@other)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
   # test "the truth" do
   #   assert true
