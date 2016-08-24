a5dc5bff9478228b621dbcd5edc103ebbede69fc
Remotezen <remotezen@bell.net>
2016-08-24 15:34:02 -0400

    finished following user tests

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class RelationshipsControllerTest < ActionDispatch::IntegrationTest
-  test ""
+  test "create should require a logged in user" do
+    assert_no_difference 'Relationship.count' do
+      post relationships_path
+    end
+    assert_redirected_to login_url
+  end
+
+  test "destroy should require logged in user" do
+    assert_no_difference 'Relationship.count' do
+      delete relationship_path(relationships(:fred_following))
+    end
+  end
   # test "the truth" do
   #   assert true
   # end
--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class FollowingTest < ActionDispatch::IntegrationTest
+
   def setup
     @user = users(:fred)
+    @other = users(:archer)
     log_in_as(@user)
   end
 
@@ -24,7 +26,36 @@ class FollowingTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "should follow a user the standard way" do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, params: {followed_id: @other.id}
+    end
+  end
+
+  test "should follow a user with Ajax" do
+    assert_difference '@user.following.count',1 do
+      post relationships_path xhr: true, params: {followed_id: @other.id}
+    end
+  end
+
+ test "should unfollow a user the standard way" do
+  @user.follow(@other)
+  relationship = @user.active_relationships.find_by(followed_id: @other.id)
+   assert_difference '@user.following.count', -1 do
+     delete relationship_path(relationship)
+   end
+  end
+
+ test "should unfollow a user the Ajax way" do
+  @user.follow(@other)
+  relationship = @user.active_relationships.find_by(followed_id: @other.id)
+   assert_difference '@user.following.count', -1 do
+     delete relationship_path(relationship), xhr: true
+   end
+  end
+end
+
+
   # test "the truth" do
   #   assert true
   # end
-end
