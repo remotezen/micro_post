acf70cd2263cef37b7ee04cac241555ac26793ea
Remotezen <remotezen@bell.net>
2016-08-24 14:21:44 -0400

    generated integration test following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -46,5 +46,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_path
   end
 
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
+
 
 end
