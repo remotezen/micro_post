5ae3bdd2a6e21eb4ad40563cbc7cc85b07562a1f
Remotezen <remotezen@bell.net>
2016-08-24 14:48:12 -0400

    added integration following test

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -1,6 +1,20 @@
 require 'test_helper'
 
 class FollowingTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:fred)
+    log_in_as(@user)
+  end
+
+  test "following page" do
+    get following_user_path(@user)
+    assert_not @user.following.empty?
+    assert_match @user.following.count.to_s, response.body
+    @user.following.each do |u|
+      assert_select "a[href=?]", user_path(u)
+    end
+
+  end
   # test "the truth" do
   #   assert true
   # end
